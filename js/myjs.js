35d18866ec1159b3e9851838b9ca4f2ac7a18f60
Omaira Pizan Verastegui <opizanv13@unc.edu.pe>
2018-10-12 08:40:13 -0500

    comit 5

---
+++ js/myjs.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var flag = 0;
+
+
+    document.getElementById("btn").onclick = function() {
+
+        var pokeballImg = document.getElementById("icon01");
+        if (flag == 0) {
+            pokeballImg.src = "/imagenes/pokela2.jpg";
+            flag = 1;
+        } else {
+            pokebolaImg.src = "/imagenes/pokebola1.jpg";
+            flag = 0;
+        }
+    }
+}());
